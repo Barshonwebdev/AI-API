ab7673c6b63db36bd605993bd986da396f5e5639
Barshon <barshonweb@gmail.com>
2023-08-31 12:51:59 +0600

    footer bg color corrected

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const displayData = (ais) => {
                   longer.
                 </p>
               </div>
-              <div class="card-footer d-flex justify-content-between align-items-center">
+              <div class="card-footer bg-white d-flex justify-content-between align-items-center">
               <div>
                 <h5 class=""
                   >${ai.name}
