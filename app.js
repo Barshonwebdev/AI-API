387174b32187fa5898b8deaa7f4636f7f5578f47
Barshon <barshonweb@gmail.com>
2023-08-31 20:19:35 +0600

    individual details data loaded

--- app.js
+++ app.js
@@ -4,29 +4,27 @@ const loadData = async () => {
   const res = await fetch(url);
   const data = await res.json();
   const allData = data.data.tools;
-  console.log(data.data.tools);
   displayData(allData);
 };
 
-// all data loading 
+// all data loading
 const loadAllData = async () => {
   toggleSpinner(true);
   const url = "https://openapi.programming-hero.com/api/ai/tools";
   const res = await fetch(url);
   const data = await res.json();
   const allData = data.data.tools;
-  console.log(data.data.tools);
+  console.log(allData);
   displayAllData(allData);
   toggleSpinner(false);
-  const buttonBox=document.getElementById("see-more-container");
+  const buttonBox = document.getElementById("see-more-container");
   buttonBox.classList.add("d-none");
 };
 
-// displaying data of 6 
+// displaying data of 6
 const displayData = (ais) => {
-  console.log(ais);
   const aiContainer = document.getElementById("ai-box-container");
-  arrayForSix=ais.slice(0,7);
+  arrayForSix = ais.slice(0, 7);
   arrayForSix.forEach((ai) => {
     if (ai.id === "06") {
       return;
@@ -34,12 +32,14 @@ const displayData = (ais) => {
     const eachBox = document.createElement("div");
     eachBox.classList.add("col");
     eachBox.innerHTML = `<div class="card h-100">
-              <img src="${ai.image}" class="card-img-top p-3 rounded-5" alt="..." />
+              <img src="${
+                ai.image
+              }" class="card-img-top p-3 rounded-5" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">Features</h5>
                 <p>1. ${ai.features[0]}</p>
                 <p>2. ${ai.features[1]}</p>
-                <p>3. ${ai.features[2]? ai.features[2]:"Fast Service"}</p>
+                <p>3. ${ai.features[2] ? ai.features[2] : "Fast Service"}</p>
               </div>
               <div class="card-footer bg-white d-flex justify-content-between align-items-center">
               <div>
@@ -51,7 +51,9 @@ const displayData = (ais) => {
                 </p>
               </div>
               <div>
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal">View Details</button>
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#detailModal" onclick="loadDetail('${
+                  ai.id
+                }')">View Details</button>
               </div>
               </div>
             </div> `;
@@ -59,11 +61,10 @@ const displayData = (ais) => {
   });
 };
 
-// displaying all data 
+// displaying all data
 const displayAllData = (ais) => {
-  console.log(ais);
   const aiContainer = document.getElementById("ai-box-container");
-  aiContainer.innerHTML="";
+  aiContainer.innerHTML = "";
   ais.forEach((ai) => {
     if (ai.id === "06") {
       return;
@@ -90,7 +91,9 @@ const displayAllData = (ais) => {
                 </p>
               </div>
               <div>
-                <button data-bs-toggle="modal" data-bs-target="#detailModal" class="btn btn-primary">View Details</button>
+                <button data-bs-toggle="modal" data-bs-target="#detailModal" class="btn btn-primary" onclick="loadDetail('${
+                  ai.id
+                }')">View Details</button>
               </div>
               </div>
             </div> `;
@@ -98,27 +101,25 @@ const displayAllData = (ais) => {
   });
 };
 
-// load detail data 
-const loadDetail=async id=>{
-  const url= `https://openapi.programming-hero.com/api/ai/tool/${id}`;
-  const res= await fetch(url);
-  const data= await res.json();
+// load detail data
+const loadDetail = async (id) => {
+  const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+  const res = await fetch(url);
+  const data = await res.json();
   console.log(data);
-}
+};
 
-loadDetail("03");
-// toggle spinner function 
+// toggle spinner function
 
-const toggleSpinner=isLoading=>{
-  const spinner=document.getElementById("spinner");
-  if(isLoading){
+const toggleSpinner = (isLoading) => {
+  const spinner = document.getElementById("spinner");
+  if (isLoading) {
     spinner.classList.remove("d-none");
     spinner.classList.add("d-block");
-  }
-  else {
+  } else {
     spinner.classList.remove("d-block");
     spinner.classList.add("d-none");
   }
-}
-// initial loading 
+};
+// initial loading
 loadData();
