ab864f170f791e98619d62484e37383ceb50f2f1
Barshon <barshonweb@gmail.com>
2023-08-31 12:49:47 +0600

    name, release date and view details button added in each box

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-// data loading 
-const loadData=async()=>{
-    const url = "https://openapi.programming-hero.com/api/ai/tools";
-    const res= await fetch(url);
-    const data= await res.json();
-    const allData=data.data.tools;
-    console.log(data.data.tools);
-    displayData(allData);
-}
+// data loading
+const loadData = async () => {
+  const url = "https://openapi.programming-hero.com/api/ai/tools";
+  const res = await fetch(url);
+  const data = await res.json();
+  const allData = data.data.tools;
+  console.log(data.data.tools);
+  displayData(allData);
+};
 
-const displayData=ais=>{
-    console.log(ais);
-    const aiContainer=document.getElementById("ai-box-container");
-    ais.forEach(ai=>{
-        if(ai.id==="06"){
-            return;
-        }
-        const eachBox=document.createElement("div");
-        eachBox.classList.add("col");
-        eachBox.innerHTML = `<div class="card h-100">
+const displayData = (ais) => {
+  console.log(ais);
+  const aiContainer = document.getElementById("ai-box-container");
+  ais.forEach((ai) => {
+    if (ai.id === "06") {
+      return;
+    }
+    const eachBox = document.createElement("div");
+    eachBox.classList.add("col");
+    eachBox.innerHTML = `<div class="card h-100">
               <img src="${ai.image}" class="card-img-top p-3 rounded-5" alt="..." />
               <div class="card-body">
                 <h5 class="card-title">Features</h5>
@@ -27,14 +27,22 @@ const displayData=ais=>{
                   longer.
                 </p>
               </div>
-              <div class="card-footer">
-                <small class="text-body-secondary"
-                  >Last updated 3 mins ago</small
-                >
+              <div class="card-footer d-flex justify-content-between align-items-center">
+              <div>
+                <h5 class=""
+                  >${ai.name}
+                </h5>
+                <p>
+                Release: ${ai.published_in}
+                </p>
+              </div>
+              <div>
+                <button class="btn btn-primary">View Details</button>
+              </div>
               </div>
             </div> `;
-        aiContainer.appendChild(eachBox);
-    });
-}
+    aiContainer.appendChild(eachBox);
+  });
+};
 
 loadData();
