18dc4a50487d558dc98523799baab431a8c420eb
Barshon <barshonweb@gmail.com>
2023-08-31 12:09:11 +0600

    box for all data displayed

--- app.js
+++ app.js
@@ -3,7 +3,35 @@ const loadData=async()=>{
     const url = "https://openapi.programming-hero.com/api/ai/tools";
     const res= await fetch(url);
     const data= await res.json();
-    console.log(data.data);
+    const allData=data.data.tools;
+    console.log(data.data.tools);
+    displayData(allData);
+}
+
+const displayData=ais=>{
+    console.log(ais);
+    const aiContainer=document.getElementById("ai-box-container");
+    ais.forEach(ai=>{
+        const eachBox=document.createElement("div");
+        eachBox.classList.add("col");
+        eachBox.innerHTML = `<div class="card h-100">
+              <img src="..." class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+              </div>
+              <div class="card-footer">
+                <small class="text-body-secondary"
+                  >Last updated 3 mins ago</small
+                >
+              </div>
+            </div> `;
+        aiContainer.appendChild(eachBox);
+    });
 }
 
 loadData();
